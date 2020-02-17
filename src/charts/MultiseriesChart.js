9e7796ce56cce91b987c0cff2f16f6ed08c5e52c
samhkwest <samhkwest@gmail.com>
2020-02-17 15:47:15 +0800

    Fix interval

--- src/charts/MultiseriesChart.js
+++ src/charts/MultiseriesChart.js
@@ -7,6 +7,7 @@ const initialState = {
 	disabled: false,
 	totalTime: 5, //total time to count no of clicks
 	intCnt: 10, //time interval count
+	timeInt: 0.5,
 	minus: 0,
 	plus: 0,
 	orange: 0,
@@ -27,20 +28,9 @@ class MultiseriesChart extends Component {
 		this.resetAll = this.resetAll.bind(this);
 	}
 
-	  /*
-	startTimer = () => {
-		if (this.state.index === 1) {
-			this.setState({ timerOn: 1 });
-			var intId = this.interval = setInterval(() => this.process(), (this.state.totalTime/this.state.intCnt) * 1000);
-			this.setState({intervalId: intId});
-		}else if (this.state.index === this.state.intCnt){
-			clearInterval(this.state.intervalId);
-		}
-	};*/
-
 	process() {
 		//this.setState({ index: this.state.index + 1 });
-		console.log("Index: "+this.state.index+", Time: " +this.state.index*(this.state.totalTime/this.state.intCnt)+ ", Total Time: "+this.state.totalTime+", Orange: "+this.state.orange + ", Blue: " + this.state.blue);
+		console.log("Index: "+this.state.index+", Time: " +this.state.index*this.state.timeInt+ ", Total Time: "+this.state.totalTime+", Orange: "+this.state.orange + ", Blue: " + this.state.blue);
 		//this.refreshChart(this.state.minusCnts,	this.state.plusCnts);
 		if (this.state.index <= this.state.intCnt) {
 			this.saveButtonClicks(this.state.index);
@@ -59,7 +49,7 @@ class MultiseriesChart extends Component {
 			console.log("minusCnts: "+JSON.stringify(this.state.minusCnts, null, 2));
 			console.log("plusCnts: "+JSON.stringify(this.state.plusCnts, null, 2));
 			this.refreshChart(this.state.minusCnts,	this.state.plusCnts);				
-			//console.log("Before draw char, Time: " +this.state.index*(this.state.totalTime/this.state.intCnt)+ ", Orange: "+this.state.orange + ", Blue: " + this.state.blue);
+			//console.log("Before draw char, Time: " +this.state.index*this.state.timeInt+ ", Orange: "+this.state.orange + ", Blue: " + this.state.blue);
 		}
 
 		this.setState({ minus: 0 });
@@ -82,7 +72,7 @@ class MultiseriesChart extends Component {
 			var interval = this.state.totalTime/this.state.intCnt;
 			console.log("Interval: "+interval);
 
-			var intId = this.interval = setInterval(() => this.process(), 1 * 1000);
+			var intId = this.interval = setInterval(() => this.process(), this.state.timeInt * 1000);
 			this.setState({intervalId: intId});
 		//}else if (this.state.index === this.state.intCnt){
 		//	clearInterval(this.state.intervalId);
@@ -90,27 +80,23 @@ class MultiseriesChart extends Component {
 	}
 
 	clickMinus = () => {
-		//if (!this.state.disabled) {
-			if (this.state.index <= this.state.intCnt){
-				this.setState({
-					minus: this.state.minus + 1,
-					orange: this.state.orange + 1
-				});
-			}
-			this.startTimer();
-		//}
+		if (this.state.index <= this.state.intCnt){
+			this.setState({
+				minus: this.state.minus + 1,
+				orange: this.state.orange + 1
+			});
+		}
+		this.startTimer();
 	};
 
 	clickPlus = () => {
-		//if (!this.state.disabled) {
-			if (this.state.index <= this.state.intCnt){
-				this.setState({
-					plus: this.state.plus + 1,
-					blue: this.state.blue + 1
-				});
-			}
-			this.startTimer();
-		//}
+		if (this.state.index <= this.state.intCnt){
+			this.setState({
+				plus: this.state.plus + 1,
+				blue: this.state.blue + 1
+			});
+		}
+		this.startTimer();
 	};
 
 	getCurDteTme = () => {
@@ -122,7 +108,8 @@ class MultiseriesChart extends Component {
 
 	saveButtonClicks =(i) => {
 		//if (i>0){
-			var index = i*this.state.totalTime/this.state.intCnt;
+			//console.log("saveButtonClicks.timeInt: "+this.state.timeInt);
+			var index = i*this.state.timeInt;
 			this.state.minusCnts.push({y: this.state.minus, label: index});
 			this.state.plusCnts.push({y: this.state.plus, label: index});
 		//}
@@ -167,8 +154,9 @@ class MultiseriesChart extends Component {
 	};
 
 	changeTotalTime =(e)=> {
-		console.log("Time to count clicks: "+e.target.value);
 		this.setState({totalTime: e.target.value});
+		this.setState({timeInt: e.target.value/this.state.intCnt});
+		console.log("totalTime: "+e.target.value+", timeInt: "+e.target.value/this.state.intCnt);
 	}
 
 	resetAll =()=>{
